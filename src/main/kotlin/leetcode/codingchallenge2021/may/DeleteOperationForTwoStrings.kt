813dc2f1a524c1008d6f2ccc59f131f313766c60
ydash <52343777+ydash@users.noreply.github.com>
2021-05-08 05:34:41 +0900

    chore

--- src/main/kotlin/leetcode/codingchallenge2021/may/DeleteOperationForTwoStrings.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/may/DeleteOperationForTwoStrings.kt
@@ -19,7 +19,7 @@ class DeleteOperationForTwoStrings {
             (1..n).forEach { j ->
                 if (word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1]
                 else dp[i][j] =
-                    kotlin.math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                    kotlin.math.min(dp[i - 1][j], dp[i][j - 1]) + 1
             }
         }
 
