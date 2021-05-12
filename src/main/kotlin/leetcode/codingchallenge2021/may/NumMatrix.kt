8870a82a5773a65bbd02bd83cd17fd0aa5c0378d
ydash <52343777+ydash@users.noreply.github.com>
2021-05-13 00:54:21 +0900

    最適化

--- src/main/kotlin/leetcode/codingchallenge2021/may/NumMatrix.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/may/NumMatrix.kt
@@ -1,11 +1,16 @@
 package leetcode.codingchallenge2021.may
 
 class NumMatrix(matrix: Array<IntArray>) {
-    private val m = matrix
-    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int =
-        (row1..row2).fold(0) { acc1, i ->
-            (col1..col2).fold(acc1) { acc2, j ->
-                acc2 + m[i][j]
+    private val maxRowInd = matrix.size
+    private val maxColInd = matrix[0].size
+    val dp = Array(maxRowInd + 1) { IntArray(maxColInd + 1) }.also {
+        (0 until maxRowInd).forEach { i ->
+            (0 until maxColInd).forEach { j ->
+                it[i + 1][j + 1] = matrix[i][j] + it[i + 1][j] + it[i][j + 1] - it[i][j]
             }
         }
+    }
+
+    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int =
+        dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1]
 }
