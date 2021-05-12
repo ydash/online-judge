b1c67eb702a0b4fcbdddea5619ab659c8a520180
ydash <52343777+ydash@users.noreply.github.com>
2021-05-12 23:39:04 +0900

    chore: パッケージ移動

--- src/test/kotlin/leetcode/codingchallenge2021/may/SuperPalindromesTest.kt
+++ src/test/kotlin/leetcode/codingchallenge2021/may/SuperPalindromesTest.kt
@@ -1,10 +1,9 @@
-package leetcode.problem
+package leetcode.codingchallenge2021.may
 
 import io.kotest.data.blocking.forAll
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
-import leetcode.codingchallenge2021.may.SuperPalindromes
 
 class SuperPalindromesTest : StringSpec({
     "return the number of super-palindromes integers in the inclusive range [left, right]." {
