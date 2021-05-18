ae7c94311453828324444249900e1962fa4ebf7c
ydash <52343777+ydash@users.noreply.github.com>
2021-05-18 21:40:32 +0900

    feat: implement Find Duplicate Files in System

---
+++ src/main/kotlin/leetcode/codingchallenge2021/may/FindDuplicateFileInSystem.kt
@@ -0,0 +1,21 @@
+package leetcode.codingchallenge2021.may
+
+object FindDuplicateFileInSystem {
+    fun findDuplicate(paths: Array<String>): List<List<String>> {
+        val cache = mutableMapOf<String, MutableList<String>>()
+
+        paths.forEach { path ->
+            val (dir, fileList) = path.split(' ').let { Pair(it[0], it.drop(1)) }
+            fileList.forEach { file ->
+                val (x, y) = Pair(file.lastIndexOf('('), file.lastIndexOf(')'))
+                val content = file.substring(x + 1, y)
+                val fileName = file.substring(0, x)
+                when (cache.containsKey(content)) {
+                    false -> cache += (content to mutableListOf("$dir/$fileName"))
+                    true -> cache[content]!! += "$dir/$fileName"
+                }
+            }
+        }
+        return cache.values.filter { it.size != 1 }.toList()
+    }
+}
