58d7f6f8102042e898cc0e24d0d8c32abb9d191f
ydash <52343777+ydash@users.noreply.github.com>
2021-05-20 23:23:37 +0900

    test: add test of Binary Tree Level Order Traversal

---
+++ src/test/kotlin/leetcode/codingchallenge2021/may/BinaryTreeLevelOrderTraversalTest.kt
@@ -0,0 +1,24 @@
+package leetcode.codingchallenge2021.may
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class BinaryTreeLevelOrderTraversalTest : StringSpec({
+    "Given the root of a binary tree, return the level order traversal of its node's values" {
+        forAll(
+            row(TreeNode(3).also {
+                it.left = TreeNode(9)
+                it.right = TreeNode(20).also {
+                    it.left = TreeNode(15)
+                    it.right = TreeNode(7)
+                }
+            }, listOf(listOf(3), listOf(9, 20), listOf(15, 7))),
+            row(TreeNode(1), listOf(listOf(1))),
+            row(null, listOf())
+        ) { root, expected ->
+            BinaryTreeLevelOrderTraversal.levelOrder(root) shouldBe expected
+        }
+    }
+})
