1413449d3986de9da3919a7165e8e03ed7149c61
ydash <52343777+ydash@users.noreply.github.com>
2021-05-23 22:46:19 +0900

    test: refactor test assertions

--- src/test/kotlin/leetcode/FindTheShortestSuperStringTest.kt
+++ src/test/kotlin/leetcode/FindTheShortestSuperStringTest.kt
@@ -9,10 +9,14 @@ import leetcode.codingchallenge2021.may.FindTheShortestSuperString
 class FindTheShortestSuperStringTest : StringSpec({
     "Given an array of strings `words`, return the smallest string that contains each string in words as a substring." {
         forAll(
-            row(arrayOf("alex", "loves", "leetcode"), "alexlovesleetcode"),
-            row(arrayOf("catg", "ctaagt", "gcta", "ttca", "atgcatc"), "gctaagttcatgcatc")
-        ) { input, output ->
-            FindTheShortestSuperString.shortestSuperstring(input) shouldBe output
+            row(arrayOf("alex", "loves", "leetcode"), 17),
+            row(arrayOf("catg", "ctaagt", "gcta", "ttca", "atgcatc"), 16),
+            row(arrayOf("aaa", "aab", "abb", "bba", "bbb"), 7),
+            row(arrayOf("abcdef", "efde", "defab"), 11)
+        ) { input, expectedLength ->
+            val result = FindTheShortestSuperString.shortestSuperstring(input)
+            result.length shouldBe expectedLength
+            input.all { result.contains(it) } shouldBe true
         }
     }
 })
