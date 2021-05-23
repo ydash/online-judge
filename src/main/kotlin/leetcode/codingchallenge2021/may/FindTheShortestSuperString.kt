da179c176b7dd399ada2fc438c06b31eb6389943
ydash <52343777+ydash@users.noreply.github.com>
2021-05-23 22:53:58 +0900

    refactor: remove redundant nest

--- src/main/kotlin/leetcode/codingchallenge2021/may/FindTheShortestSuperString.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/may/FindTheShortestSuperString.kt
@@ -3,29 +3,27 @@ package leetcode.codingchallenge2021.may
 object FindTheShortestSuperString {
     fun shortestSuperstring(words: Array<String>): String = greedy(words.toSet())
 
-    fun greedy(words: Set<String>): String =
-        if (words.size <= 1) words.first()
-        else {
-            val m = mutableMapOf<Int, MutableList<Triple<String, Int, Int>>>()
-            val tmp = words.toTypedArray()
+    fun greedy(words: Set<String>): String {
+        val m = mutableMapOf<Int, MutableList<Triple<String, Int, Int>>>()
+        val tmp = words.toTypedArray()
 
-            tmp.indices.forEach { i ->
-                ((i + 1)..tmp.lastIndex).forEach { j ->
-                    val (len, str) = overlap(tmp[i], tmp[j])
-                    when (val v = m[len]) {
-                        null -> m += (len to mutableListOf(Triple(str, i, j)))
-                        else -> v += Triple(str, i, j)
-                    }
-                }
+        tmp.indices.forEach { i ->
+            ((i + 1)..tmp.lastIndex).forEach { j ->
+                val (len, str) = overlap(tmp[i], tmp[j])
+                m[len]?.let { it += Triple(str, i, j) }
+                    ?: run { m += (len to mutableListOf(Triple(str, i, j))) }
             }
+        }
 
-            val maxOverlap = m.maxByOrNull { it.key }!!.key
+        val maxOverlap = m.maxByOrNull { it.key }?.key ?: 0
+        return if (maxOverlap == 0) words.joinToString("")
+        else {
             val threshold = if (maxOverlap >= 2) maxOverlap - 1 else maxOverlap
-            if (maxOverlap == 0) words.joinToString("")
-            else m.filterKeys { it >= threshold }.values.flatten()
+            m.filterKeys { it >= threshold }.values.flatten()
                 .map { (str, x, y) -> greedy(words - tmp[x] - tmp[y] + str) }
                 .minByOrNull { it.length }!!
         }
+    }
 
     fun overlap(str1: String, str2: String): Pair<Int, String> {
         var max = 0
