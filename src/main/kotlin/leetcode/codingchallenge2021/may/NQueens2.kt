e6ec7393b043d64f5f5649f0754a4d66f0f1c407
ydash <52343777+ydash@users.noreply.github.com>
2021-05-30 02:38:28 +0900

    fix typo

--- src/main/kotlin/leetcode/codingchallenge2021/may/NQueens2.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/may/NQueens2.kt
@@ -5,21 +5,21 @@ object NQueens2 {
         var count = 0
         val occupiedRightDiag = BooleanArray(n * 2 - 1)
         val occupiedLeftDiag = BooleanArray(n * 2 - 1)
-        val occupidRow = BooleanArray(n)
+        val occupiedRow = BooleanArray(n)
 
         fun backtracking(col: Int) {
             (0 until n).forEach loop@{ row ->
                 val leftDiag = col + row
                 val rightDiag = n - col + row - 1
-                if (occupidRow[row] || occupiedLeftDiag[leftDiag] || occupiedRightDiag[rightDiag]) return@loop
+                if (occupiedRow[row] || occupiedLeftDiag[leftDiag] || occupiedRightDiag[rightDiag]) return@loop
                 if (col == n - 1) {
                     count++
                 } else {
-                    occupidRow[row] = true
+                    occupiedRow[row] = true
                     occupiedLeftDiag[leftDiag] = true
                     occupiedRightDiag[rightDiag] = true
                     backtracking(col + 1)
-                    occupidRow[row] = false
+                    occupiedRow[row] = false
                     occupiedLeftDiag[leftDiag] = false
                     occupiedRightDiag[rightDiag] = false
                 }
