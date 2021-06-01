6d26419305a62cbfb9d222118b67dec8753db10d
ydash <52343777+ydash@users.noreply.github.com>
2021-06-02 00:59:45 +0900

    add test of Max Area of Island

---
+++ src/test/kotlin/leetcode/codingchallenge2021/may/MaxAreaOfIslandTest.kt
@@ -0,0 +1,32 @@
+package leetcode.codingchallenge2021.may
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class MaxAreaOfIslandTest : StringSpec({
+    "Given binary matrix, return the maximum area of an island which is the number of cells with a value 1." {
+        forAll(
+            row(
+                arrayOf(
+                    intArrayOf(0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+                    intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+                    intArrayOf(0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+                    intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0),
+                    intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0),
+                    intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+                    intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+                    intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0)
+                ),
+                6
+            ),
+            row(
+                arrayOf(intArrayOf(0, 0, 0, 0, 0, 0, 0, 0)),
+                0
+            )
+        ) { grid, expected ->
+            MaxAreaOfIslandSolution.maxAreaOfIsland(grid) shouldBe expected
+        }
+    }
+})
