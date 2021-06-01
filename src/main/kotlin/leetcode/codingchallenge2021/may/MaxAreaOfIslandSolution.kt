2c22474a06f15b504768d2ee5cf223fea491842c
ydash <52343777+ydash@users.noreply.github.com>
2021-06-02 01:00:10 +0900

    add implementation of Max Area of Island

---
+++ src/main/kotlin/leetcode/codingchallenge2021/may/MaxAreaOfIslandSolution.kt
@@ -0,0 +1,25 @@
+package leetcode.codingchallenge2021.may
+
+object MaxAreaOfIslandSolution {
+    fun maxAreaOfIsland(grid: Array<IntArray>): Int {
+        var ans = 0
+        val m = grid.size
+        val n = grid[0].size
+
+        fun dfs(i: Int, j: Int): Int = when (grid.getOrNull(i)?.getOrNull(j)) {
+            null, 0 -> 0
+            else -> {
+                grid[i][j] = 0
+                1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+            }
+        }
+
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (grid[i][j] == 1) ans = maxOf(ans, dfs(i, j))
+            }
+        }
+
+        return ans
+    }
+}
