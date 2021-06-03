ebe60d3b24f8e6ddd6996cf9596ff2def6afb3f7
ydash <52343777+ydash@users.noreply.github.com>
2021-06-04 03:18:27 +0900

    add accepted solution

--- src/main/kotlin/leetcode/codingchallenge2021/may/MaximumAreaofaPieceofCake.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/may/MaximumAreaofaPieceofCake.kt
@@ -4,35 +4,23 @@ const val DIVISOR = 1000000007
 
 object MaximumAreaofaPieceofCake {
     fun maxArea(h: Int, w: Int, horizontalCuts: IntArray, verticalCuts: IntArray): Int {
-        var ans = 0
-        val current = IntArray(w) { 1 }
-        val prev = IntArray(w)
-        val cutH = BooleanArray(h).apply {
-            horizontalCuts.forEach { this[it] = true }
-        }
-        val cutV = BooleanArray(w).apply {
-            verticalCuts.forEach {
-                this[it] = true
-            }
-        }
-        for (i in 0 until h) {
-            for (j in 0 until w) {
-                when {
-                    i == 0 && j == 0 -> current[0] = 1
-                    i == 0 -> current[j] = if (!cutV[j]) current[j - 1] + 1 else 1
-                    j == 0 -> current[j] = if (!cutH[i]) prev[j] + 1 else 1
-                    cutH[i] && cutV[j] -> current[j] = 1
-                    cutH[i] -> current[j] = current[j - 1] + 1
-                    cutV[j] -> current[j] = prev[j] + 1
-                    else -> current[j] = current[j - 1] - prev[j - 1] + prev[j] + 1
-                }
-            }
-            current.indices.forEach {
-                prev[it] = current[it] % DIVISOR
-                ans = maxOf(ans, prev[it])
-            }
+        horizontalCuts.sort()
+        verticalCuts.sort()
+        var maxH = 0
+        var maxW = 0
+
+        maxH = maxOf(maxH, horizontalCuts[0])
+        maxH = maxOf(maxH, h - horizontalCuts.last())
+        horizontalCuts.indices.drop(1).forEach { i ->
+            maxH = maxOf(maxH, horizontalCuts[i] - horizontalCuts[i - 1])
         }
 
-        return ans
+        maxW = maxOf(maxW, verticalCuts[0])
+        maxW = maxOf(maxW, w - verticalCuts.last())
+        verticalCuts.indices.drop(1).forEach { i ->
+            maxW = maxOf(maxW, verticalCuts[i] - verticalCuts[i - 1])
+        }
+
+        return ((maxH.toLong() * maxW) % DIVISOR).toInt()
     }
 }
