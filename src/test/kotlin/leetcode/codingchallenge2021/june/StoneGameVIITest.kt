be68bfbd3e547aab035608cf20243a8b70cce0b9
ydash <52343777+ydash@users.noreply.github.com>
2021-06-12 23:02:46 +0900

    add test of Stone Game VII

---
+++ src/test/kotlin/leetcode/codingchallenge2021/june/StoneGameVIITest.kt
@@ -0,0 +1,17 @@
+package leetcode.codingchallenge2021.june
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class StoneGameVIITest : StringSpec({
+    "test of Stone Game VII" {
+        forAll(
+            row(intArrayOf(5, 3, 1, 4, 2), 6),
+            row(intArrayOf(7, 90, 5, 1, 100, 10, 10, 2), 122)
+        ) { input, output ->
+            StoneGameVII.stoneGameVII(input) shouldBe output
+        }
+    }
+})
