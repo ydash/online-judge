a167a2968162d487628be71874596ed5dcfd8583
ydash <52343777+ydash@users.noreply.github.com>
2021-06-13 04:10:56 +0900

    optimize the solution of Minimum Refueling Stops by using heap

--- src/main/kotlin/leetcode/codingchallenge2021/june/MinimumRefuelingStops.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/june/MinimumRefuelingStops.kt
@@ -1,30 +1,23 @@
 package leetcode.codingchallenge2021.june
 
+import java.util.*
+
 object MinimumRefuelingStops {
-    fun minRefuelStops(target: Int, startFuel: Int, stations: Array<IntArray>): Int =
-        when {
-            stations.isEmpty() && startFuel < target -> -1
-            startFuel >= target -> 0
-            else -> {
-                val n = stations.size
-                val prev = IntArray(n + 1) { startFuel }
-                val current = IntArray(n + 1)
-                var ans = -1
-                loop@ for (i in 1..n) {
-                    for (j in i..n) {
-                        if (prev[j - 1] >= stations[j - 1][0]) current[j] = prev[j - 1] + stations[j - 1][1]
-                        if (current[j] >= target) {
-                            ans = i
-                            break@loop
-                        }
-                        current[j] = maxOf(current[j], current[j - 1])
-                    }
-                    (0..n).forEach {
-                        prev[it] = current[it]
-                        current[it] = 0
-                    }
-                }
-                ans
+    fun minRefuelStops(target: Int, startFuel: Int, stations: Array<IntArray>): Int {
+        val n = stations.size
+        val pq = PriorityQueue<Int> { a, b -> b - a }
+        var count = 0
+        var current = startFuel
+        var i = 0
+        while (current < target) {
+            while (i < n && stations[i][0] <= current) pq.offer(stations[i++][1])
+            if (pq.isEmpty()) {
+                count = -1
+                break
             }
+            current += pq.poll()
+            ++count
         }
+        return count
+    }
 }
