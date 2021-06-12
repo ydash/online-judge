ea8b747bfd293e568a525f84b9f26e921ec23d72
ydash <52343777+ydash@users.noreply.github.com>
2021-06-12 23:03:02 +0900

    add solution of Stone Game VII

---
+++ src/main/kotlin/leetcode/codingchallenge2021/june/StoneGameVII.kt
@@ -0,0 +1,23 @@
+package leetcode.codingchallenge2021.june
+
+object StoneGameVII {
+    fun stoneGameVII(stones: IntArray): Int {
+        val n = stones.size
+        val currentDp = IntArray(n)
+        val prevDp = IntArray(n)
+
+        (n - 2 downTo 0).forEach { i ->
+            var total = stones[i]
+            val tmp = prevDp.copyOf()
+            (0 until n).forEach { ind -> prevDp[ind] = currentDp[ind] }
+            (0 until n).forEach { ind -> currentDp[ind] = tmp[ind] }
+
+            (i + 1 until n).forEach { j ->
+                total += stones[j]
+                currentDp[j] = maxOf(total - stones[i] - prevDp[j], total - stones[j] - currentDp[j - 1])
+            }
+        }
+
+        return currentDp.last()
+    }
+}
