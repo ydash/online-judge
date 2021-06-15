c55bf75d742605eefc2f1b503abb084e339c22cf
ydash <52343777+ydash@users.noreply.github.com>
2021-06-16 01:29:00 +0900

    add solution of Matchsticks to Square

---
+++ src/main/kotlin/leetcode/codingchallenge2021/june/MatchsticksSquare.kt
@@ -0,0 +1,19 @@
+package leetcode.codingchallenge2021.june
+
+object MatchsticksSquare {
+    fun makesquare(matchsticks: IntArray): Boolean {
+        val sum = matchsticks.sum()
+        if (sum % 4 != 0) return false
+        val len = sum / 4
+        for (l in matchsticks) if (l > len) return false
+
+        fun loop(i: Int, a: Int, b: Int, c: Int, d: Int): Boolean =
+            if (i >= matchsticks.size) a == b && b == c && c == d
+            else (a + matchsticks[i] <= len && loop(i + 1, a + matchsticks[i], b, c, d)) ||
+                    (b + matchsticks[i] <= len && loop(i + 1, a, b + matchsticks[i], c, d)) ||
+                    (c + matchsticks[i] <= len && loop(i + 1, a, b, c + matchsticks[i], d)) ||
+                    (d + matchsticks[i] <= len && loop(i + 1, a, b, c, d + matchsticks[i]))
+
+        return loop(0, 0, 0, 0, 0)
+    }
+}
