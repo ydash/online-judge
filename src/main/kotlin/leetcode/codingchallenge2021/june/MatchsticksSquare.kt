edf4f23b8c87398ea24603d70cceda03a6e6cee3
ydash <52343777+ydash@users.noreply.github.com>
2021-06-16 03:24:59 +0900

    optimize the solution of Matchsticks to Square

--- src/main/kotlin/leetcode/codingchallenge2021/june/MatchsticksSquare.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/june/MatchsticksSquare.kt
@@ -4,15 +4,17 @@ object MatchsticksSquare {
     fun makesquare(matchsticks: IntArray): Boolean {
         val sum = matchsticks.sum()
         if (sum % 4 != 0) return false
-        val len = sum / 4
-        for (l in matchsticks) if (l > len) return false
+        val target = sum / 4
+        for (l in matchsticks) if (l > target) return false
+
+        matchsticks.sortDescending()
 
         fun loop(i: Int, a: Int, b: Int, c: Int, d: Int): Boolean =
             if (i >= matchsticks.size) a == b && b == c && c == d
-            else (a + matchsticks[i] <= len && loop(i + 1, a + matchsticks[i], b, c, d)) ||
-                    (b + matchsticks[i] <= len && loop(i + 1, a, b + matchsticks[i], c, d)) ||
-                    (c + matchsticks[i] <= len && loop(i + 1, a, b, c + matchsticks[i], d)) ||
-                    (d + matchsticks[i] <= len && loop(i + 1, a, b, c, d + matchsticks[i]))
+            else (a + matchsticks[i] <= target && loop(i + 1, a + matchsticks[i], b, c, d)) ||
+                    (b + matchsticks[i] <= target && loop(i + 1, a, b + matchsticks[i], c, d)) ||
+                    (c + matchsticks[i] <= target && loop(i + 1, a, b, c + matchsticks[i], d)) ||
+                    (d + matchsticks[i] <= target && loop(i + 1, a, b, c, d + matchsticks[i]))
 
         return loop(0, 0, 0, 0, 0)
     }
