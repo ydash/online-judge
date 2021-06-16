9dacbfdbfc3a4f5ee09828b3421b51c1c6875657
ydash <52343777+ydash@users.noreply.github.com>
2021-06-17 01:53:22 +0900

    add test of Generate Parentheses

---
+++ src/test/kotlin/leetcode/codingchallenge2021/june/GenerateParenthesesTest.kt
@@ -0,0 +1,19 @@
+package leetcode.codingchallenge2021.june
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class GenerateParenthesesTest : StringSpec({
+    "test of Generate Parentheses" {
+        forAll(
+            row(1, listOf("()")),
+            row(3, listOf("((()))", "(()())", "(())()", "()(())", "()()()")),
+            row(4, listOf("(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"))
+        ) { num, expected ->
+            val actual = GenerateParentheses.generateParenthesis(num)
+                actual shouldBe expected
+        }
+    }
+})
