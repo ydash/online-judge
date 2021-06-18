8fb01212d1577100e0b04f2bb2d7af7756f3c8d3
ydash <52343777+ydash@users.noreply.github.com>
2021-06-19 03:34:04 +0900

    add solution of Range Sum Query - Mutable

---
+++ src/main/kotlin/leetcode/codingchallenge2021/june/NumArray.kt
@@ -0,0 +1,11 @@
+package leetcode.codingchallenge2021.june
+
+import leetcode.util.SegmentTree
+
+class NumArray(nums: IntArray) {
+    private val segmentTree = SegmentTree(nums)
+
+    fun update(index: Int, `val`: Int) = this.segmentTree.update(index, `val`)
+
+    fun sumRange(left: Int, right: Int): Int = this.segmentTree.sum(left, right)
+}
