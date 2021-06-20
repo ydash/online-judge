6aedff24583f02c7d5cfa85edaa5e7fd24196464
ydash <52343777+ydash@users.noreply.github.com>
2021-06-20 20:22:19 +0900

    refactor the solution

--- src/main/kotlin/leetcode/codingchallenge2021/june/SwimInRisingWater.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/june/SwimInRisingWater.kt
@@ -6,25 +6,22 @@ object SwimInRisingWater {
     fun swimInWater(grid: Array<IntArray>): Int {
         val n = grid.size
         val goal = Coordinate(n - 1, n - 1)
-        val seen = mutableSetOf<Coordinate>()
+        val seen = Array(n) { BooleanArray(n) }
         val pq = PriorityQueue<Pair<Int, Coordinate>> { a, b -> a.first - b.first }
 
-        seen += Coordinate(0, 0)
+        seen[0][0] = true
         pq.add(grid[0][0] to Coordinate(0, 0))
         while (pq.isNotEmpty()) {
-            val (time, coordinate) = pq.poll()
-            if (coordinate == goal) {
-                pq.add(time to coordinate)
-                break
-            }
+            val (time, coordinate) = pq.peek()
+            if (coordinate == goal) break
 
-            val (x, y) = coordinate
+            val (x, y) = pq.poll().second
             listOf(Coordinate(x - 1, y), Coordinate(x + 1, y), Coordinate(x, y - 1), Coordinate(x, y + 1))
                 .filter { (i, j) ->
-                    i in 0 until n && j in 0 until n && Coordinate(i, j) !in seen
+                    i in 0 until n && j in 0 until n && !seen[i][j]
                 }.forEach { (i, j) ->
                     val t = if (grid[i][j] <= time) time else grid[i][j]
-                    seen += Coordinate(i, j)
+                    seen[i][j] = true
                     pq.add(t to Coordinate(i, j))
                 }
         }
