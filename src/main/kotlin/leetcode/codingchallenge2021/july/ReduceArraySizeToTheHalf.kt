5453bbcded55e3a003dfbaa2ec317863f3444275
ydash <52343777+ydash@users.noreply.github.com>
2021-07-07 00:45:49 +0900

    refactor the code for time complexity reduction

--- src/main/kotlin/leetcode/codingchallenge2021/july/ReduceArraySizeToTheHalf.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/july/ReduceArraySizeToTheHalf.kt
@@ -3,15 +3,19 @@ package leetcode.codingchallenge2021.july
 class ReduceArraySizeToTheHalf {
     fun minSetSize(arr: IntArray): Int {
         val length = arr.size
-        val count = IntArray(size = 100001)
-        arr.forEach { n -> count[n] += 1 }
+        val freq = IntArray(size = 100001)
+        val numOfFreq = IntArray(arr.size + 1)
+        arr.forEach { n -> freq[n] += 1 }
+        freq.forEach { c -> if (c != 0) numOfFreq[c] += 1 }
 
         var ans = 0
         var sum = 0
-        for (n in count.indices.sortedByDescending { count[it] }) {
-            sum += count[n]
-            ++ans
-            if (sum + sum >= length) break
+        for (n in arr.size downTo 1) {
+            repeat(numOfFreq[n]) {
+                sum += n
+                ++ans
+                if (sum + sum >= length) return ans
+            }
         }
 
         return ans
