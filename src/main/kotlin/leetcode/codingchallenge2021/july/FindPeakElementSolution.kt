77a810c3a0bf45a3b94199cdc434f91bcbcbc799
ydash <52343777+ydash@users.noreply.github.com>
2021-07-14 01:10:28 +0900

    reduce code redundancy

--- src/main/kotlin/leetcode/codingchallenge2021/july/FindPeakElementSolution.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/july/FindPeakElementSolution.kt
@@ -8,21 +8,10 @@ class FindPeakElementSolution {
         while (left < right) {
             val middle = (left + right) / 2
 
-            val up = isUp(nums, middle - 1, middle)
-            val down = isDown(nums, middle, middle + 1)
-
-            if (up && down) return middle
-            if (up) {
-                left = middle + 1
-            } else {
-                right = middle - 1
-            }
+            if (nums[middle] > nums[middle + 1]) right = middle
+            else left = middle + 1
         }
 
         return left
     }
-
-    private fun isDown(nums: IntArray, i: Int, j: Int): Boolean = j == nums.size || nums[i] > nums[j]
-
-    private fun isUp(nums: IntArray, i: Int, j: Int): Boolean = i == -1 || nums[i] < nums[j]
 }
