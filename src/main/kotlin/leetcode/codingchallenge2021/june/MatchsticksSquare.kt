c6a059c3c02ef0df064ccda09c93c8a27668562f
ydash <52343777+ydash@users.noreply.github.com>
2021-07-27 19:39:14 +0900

    reduce the complexity of the function

--- src/main/kotlin/leetcode/codingchallenge2021/june/MatchsticksSquare.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/june/MatchsticksSquare.kt
@@ -1,14 +1,20 @@
 package leetcode.codingchallenge2021.june
 
 object MatchsticksSquare {
+    private const val FOUR = 4
+
     fun makesquare(matchsticks: IntArray): Boolean {
         val sum = matchsticks.sum()
-        if (sum % 4 != 0) return false
-        val target = sum / 4
+        if (sum % FOUR != 0) return false
+        val target = sum / FOUR
         for (l in matchsticks) if (l > target) return false
 
         matchsticks.sortDescending()
 
+        return solver(matchsticks, target)
+    }
+
+    private fun solver(matchsticks: IntArray, target: Int): Boolean {
         fun loop(i: Int, a: Int, b: Int, c: Int, d: Int): Boolean =
             if (i >= matchsticks.size) a == b && b == c && c == d
             else (a + matchsticks[i] <= target && loop(i + 1, a + matchsticks[i], b, c, d)) ||
--- src/test/kotlin/leetcode/codingchallenge2021/june/StoneGameVIITest.kt
+++ src/test/kotlin/leetcode/codingchallenge2021/june/StoneGameVIITest.kt
@@ -11,7 +11,7 @@ class StoneGameVIITest : StringSpec({
             row(intArrayOf(5, 3, 1, 4, 2), 6),
             row(intArrayOf(7, 90, 5, 1, 100, 10, 10, 2), 122)
         ) { input, output ->
-            StoneGameVIISolution.stoneGameVII(input) shouldBe output
+            StoneGameVII.stoneGameVII(input) shouldBe output
         }
     }
 })
