5f084c889d646b3f624f4a67af0f7792be2cfc73
ydash <52343777+ydash@users.noreply.github.com>
2021-07-30 00:57:39 +0900

    improve space complexity by using DP

--- src/main/kotlin/leetcode/codingchallenge2021/july/ZeroOneMatrix.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/july/ZeroOneMatrix.kt
@@ -1,35 +1,32 @@
 package leetcode.codingchallenge2021.july
 
-import java.util.ArrayDeque
-
 class ZeroOneMatrix {
     fun updateMatrix(mat: Array<IntArray>): Array<IntArray> {
         val n = mat.size
         val m = mat.first().size
-        val ans = Array(n) { IntArray(m) { Int.MAX_VALUE } }
-        val queue = ArrayDeque<Pair<Int, Int>>()
+        val ans = Array(n) { IntArray(m) { DUMMY_DIST } }
+
         for (i in mat.indices) {
             for (j in mat[i].indices) {
-                if (mat[i][j] == 0) {
-                    ans[i][j] = 0
-                    queue += i to j
+                if (mat[i][j] == 0) ans[i][j] = 0
+                else {
+                    if (i > 0) ans[i][j] = minOf(ans[i][j], ans[i - 1][j] + 1)
+                    if (j > 0) ans[i][j] = minOf(ans[i][j], ans[i][j - 1] + 1)
                 }
             }
         }
 
-        while (queue.isNotEmpty()) {
-            val (i, j) = queue.removeFirst()
-            listOf(i - 1 to j, i + 1 to j, i to j - 1, i to j + 1)
-                .filter { (x, y) -> 0 <= x && 0 <= y && x < n && y < m }
-                .forEach { (x, y) ->
-                    val dist = if (mat[x][y] == 0) 0 else ans[i][j] + 1
-                    if (dist < ans[x][y]) {
-                        ans[x][y] = dist
-                        queue += x to y
-                    }
-                }
+        for (i in mat.indices.reversed()) {
+            for (j in mat[i].indices.reversed()) {
+                if (i < n - 1) ans[i][j] = minOf(ans[i][j], ans[i + 1][j] + 1)
+                if (j < m - 1) ans[i][j] = minOf(ans[i][j], ans[i][j + 1] + 1)
+            }
         }
 
         return ans
+    }
+
+    companion object {
+        const val DUMMY_DIST = 100000000
     }
 }
