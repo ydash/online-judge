09f91dd22ac06814443625232d37a0a080863e66
ydash <52343777+ydash@users.noreply.github.com>
2021-08-01 15:55:32 +0900

    reduce space complexity

--- src/main/kotlin/leetcode/codingchallenge2021/july/TrappingRainWater.kt
+++ src/main/kotlin/leetcode/codingchallenge2021/july/TrappingRainWater.kt
@@ -1,26 +1,28 @@
 package leetcode.codingchallenge2021.july
 
 class TrappingRainWater {
-    fun trap(height: IntArray): Int =
-        if (height.isEmpty()) 0 else {
-            val n = height.size
-            val left = IntArray(n)
-            val right = IntArray(n)
-            var sum = 0
+    fun trap(height: IntArray): Int {
+        if (height.isEmpty()) return 0
 
-            left[0] = height[0]
-            (1 until n).forEach { i ->
-                left[i] = maxOf(left[i - 1], height[i])
-            }
-            right[n - 1] = height.last()
-            (height.lastIndex - 1 downTo 0).forEach { i ->
-                right[i] = maxOf(right[i + 1], height[i])
-            }
+        val n = height.size
+        var left = 0
+        var right = n - 1
+        var leftMax = height[left]
+        var rightMax = height[right]
+        var sum = 0
 
-            height.indices.forEach { i ->
-                sum += minOf(left[i], right[i]) - height[i]
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) leftMax = height[left]
+                else sum += leftMax - height[left]
+                left++
+            } else {
+                if (height[right] >= rightMax) rightMax = height[right]
+                else sum += rightMax - height[right]
+                right--
             }
-
-            sum
         }
+
+        return sum
+    }
 }
